d6ce57b25fac1d53d8e4ec40357be3c314d5ec2a
Viktor Gavrilenko <viktargavrilenko@gmail.com>
2022-03-01 16:13:36 +0300

    test project is added

--- src/main/java/utils/StringUtils.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class StringUtils {
-    public static String getIdImageFromUrl(String url) {
-        return url.substring(url.lastIndexOf("-") + 1);
-    }
-}
--- src/test/java/ScanShutterTest.java
+++ src/test/java/ScanShutterTest.java
@@ -4,57 +4,39 @@ import aquality.selenium.core.utilities.JsonSettingsFile;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import pageobject.SearchPage;
 
-import java.util.Map;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 
 import static aquality.selenium.browser.AqualityServices.getBrowser;
-import static databasequeries.SearchPageQueries.addTestInfoInBase;
-import static utils.ArithmeticUtils.isMaxValue;
 import static utils.MySqlUtils.closeConnection;
 
 public class ScanShutterTest {
     private static final ISettingsFile CONFIG_FILE = new JsonSettingsFile("configData.json");
     private static final ISettingsFile TEST_FILE = new JsonSettingsFile("testData.json");
-    private static final int MAX_COUNT_IMAGES = (int) TEST_FILE.getValue("/maxCountImages");
     private static final String TYPE_SCAN = TEST_FILE.getValue("/typeScan").toString();
     private static final String DEFAULT_URL = String.format(CONFIG_FILE.getValue("/mainPage").toString(), TYPE_SCAN);
     private static final String PATH_SCREEN = System.getProperty("user.dir") + TEST_FILE.getValue("/screen").toString();
-    private int id = 0;
 
     @BeforeClass
     protected void beforeMethod() {
         getBrowser().maximize();
+        new File(PATH_SCREEN.substring(0, PATH_SCREEN.lastIndexOf("\\"))).mkdir();
     }
 
-    @Test(description = "Working with test data",
-            dataProvider = "ScannedPageNumbers", dataProviderClass = DataProviderForTests.class)
-    public void testWorkWithTestData(int page) {
-        isMaxValue(id, MAX_COUNT_IMAGES);
-        SearchPage searchPage;
-        for (int i = 0; i < 3; i++) {
-            getBrowser().goTo(DEFAULT_URL + page);
-            searchPage = new SearchPage();
-            searchPage.state().waitForDisplayed();
-            Map<String, String> data = searchPage.getMapImages(String.format(PATH_SCREEN, page));
-            Logger.getInstance().info("Data size is " + data.size());
-            if (data.size() > 0) {
-                for (Map.Entry<String, String> entry : data.entrySet()) {
-                    isMaxValue(id, MAX_COUNT_IMAGES);
-                    id++;
-                    addTestInfoInBase(entry.getKey(), String.valueOf(id), entry.getValue());
-                    Logger.getInstance().info(entry.getKey() + " " + (id) + " " + entry.getValue());
-                }
-                Logger.getInstance().info("Request number is " + i);
-                break;
-            } else {
-                try {
-                    Logger.getInstance().info("Thread sleep ");
-                    Thread.sleep(60000);
-                } catch (InterruptedException e) {
-                    Logger.getInstance().info("Error InterruptedException " + e);
-                }
-            }
+    @Test(description = "Working with test data")
+    public void testWorkWithTestData() {
+        getBrowser().goTo(DEFAULT_URL + "1");
+        byte[] bytearray = getBrowser().getScreenshot();
+        BufferedImage screen;
+        try {
+            screen = ImageIO.read(new ByteArrayInputStream(bytearray));
+            ImageIO.write(screen, "png", new File(PATH_SCREEN));
+        } catch (IOException ioException) {
+            Logger.getInstance().error("File read or write exception" + ioException);
         }
     }
 
