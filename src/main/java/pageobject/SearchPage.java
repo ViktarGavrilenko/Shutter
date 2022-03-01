5428c718b77c8b3793d2591524c3ce6bb0c972a4
Viktor Gavrilenko <viktargavrilenko@gmail.com>
2022-03-01 13:40:17 +0300

    Added getting a screenshot of error pages

--- src/main/java/pageobject/SearchPage.java
+++ src/main/java/pageobject/SearchPage.java
@@ -6,11 +6,17 @@ import aquality.selenium.elements.interfaces.ILink;
 import aquality.selenium.forms.Form;
 import org.openqa.selenium.By;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
+import static aquality.selenium.browser.AqualityServices.getBrowser;
 import static aquality.selenium.elements.ElementType.LINK;
 import static utils.StringUtils.getIdImageFromUrl;
 
@@ -24,11 +30,19 @@ public class SearchPage extends Form {
         super(By.cssSelector("div[class^='jss'] h2"), "Page Search");
     }
 
-    public Map<String, String> getMapImages() {
+    public Map<String, String> getMapImages(String pathScreen) {
         Map<String, String> listImages = new LinkedHashMap<>();
         try {
             AqualityServices.getConditionalWait().waitForTrue(() -> (images.size() > 0));
         } catch (TimeoutException e) {
+            byte[] bytearray = getBrowser().getScreenshot();
+            BufferedImage screen;
+            try {
+                screen = ImageIO.read(new ByteArrayInputStream(bytearray));
+                ImageIO.write(screen, "png", new File(pathScreen));
+            } catch (IOException ioException) {
+                Logger.getInstance().error("File read or write exception" + ioException);
+            }
             Logger.getInstance().error(IMAGE_NOT_FOUND);
             return listImages;
         }
